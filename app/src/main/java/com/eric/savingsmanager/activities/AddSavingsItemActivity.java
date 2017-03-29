59c87016a0e77c7766c86e8e4bcac2f79ccb82a7
Eric Shao <ericsen315@gmail.com>
2017-03-29 21:58:50 +0800

    Create project and AddSavingsItem screen

---
+++ app/src/main/java/com/eric/savingsmanager/activities/AddSavingsItemActivity.java
@@ -0,0 +1,15 @@
+package com.eric.savingsmanager.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eric.savingsmanager.R;
+
+public class AddSavingsItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_savings_item);
+    }
+}
