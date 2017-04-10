baa8acd91b9826f35eb71866bc9b77cfd72f465e
Eric Shao <ericsen315@gmail.com>
2017-04-10 23:08:34 +0800

    Add functions for AddSavingsItemActivity
    Main changes:
    1. Show DatePickerDialog when click on start or end date
    2. Calculate interest when data values are updated

--- app/src/main/java/com/eric/savingsmanager/activities/AddSavingsItemActivity.java
+++ app/src/main/java/com/eric/savingsmanager/activities/AddSavingsItemActivity.java
@@ -1,15 +1,176 @@
 package com.eric.savingsmanager.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.eric.savingsmanager.R;
+import com.eric.savingsmanager.utils.Constants;
+import com.eric.savingsmanager.utils.Utils;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddSavingsItemActivity extends AppCompatActivity {
+
+    // Edit text for the fields
+    private EditText mEditBankName;
+    private EditText mEditStartDate;
+    private EditText mEditEndDate;
+    private EditText mEditAmount;
+    private EditText mEditYield;
+    private EditText mEditInterest;
+
+    // Data
+    private Calendar mCalendar = Calendar.getInstance();
+    private Date mStartDate;
+    private Date mEndDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_savings_item);
+        setupUI();
     }
+
+    /**
+     * Setup UI elements
+     */
+    private void setupUI() {
+
+        // ID from layout to java
+        mEditBankName = (EditText) findViewById(R.id.edit_bank_name);
+        mEditStartDate = (EditText) findViewById(R.id.edit_start_date);
+        mEditEndDate = (EditText) findViewById(R.id.edit_end_date);
+        mEditAmount = (EditText) findViewById(R.id.edit_amount);
+        mEditYield = (EditText) findViewById(R.id.edit_yield);
+        mEditInterest = (EditText) findViewById(R.id.edit_interest);
+
+        // set text watcher to update interest
+        mEditAmount.addTextChangedListener(mInterestTextWatcher);
+        mEditYield.addTextChangedListener(mInterestTextWatcher);
+
+        // set date field listener
+        mEditStartDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    showDatePicker((EditText) v, true);
+                }
+            }
+        });
+        mEditStartDate.setInputType(InputType.TYPE_NULL);
+        mEditStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.hasFocus()) {
+                    showDatePicker((EditText) v, true);
+                }
+            }
+        });
+        mEditEndDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    showDatePicker((EditText) v, false);
+                }
+            }
+        });
+        mEditEndDate.setInputType(InputType.TYPE_NULL);
+        mEditEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.hasFocus()) {
+                    showDatePicker((EditText) v, false);
+                }
+            }
+        });
+    }
+
+
+    /**
+     * A text watcher for fields which impacts interest
+     */
+    private TextWatcher mInterestTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            updateInterest();
+        }
+    };
+
+    /**
+     * Show date picker for edit field of date
+     *
+     * @param edit date field
+     */
+    private void showDatePicker(final EditText edit, final boolean startDate) {
+        // hide keyboard
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(edit.getWindowToken(), 0);
+
+        // show date picker
+        DatePickerDialog picker = new DatePickerDialog(AddSavingsItemActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // set the date to EditText
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(year, month, dayOfMonth);
+                        Date date = new Date(calendar.getTimeInMillis());
+                        edit.setText(Utils.formatDate(date, Constants.FORMAT_DATE_YEAR_MONTH_DAY));
+                        if (startDate) {
+                            mStartDate = new Date(date.getTime());
+                        } else {
+                            mEndDate = new Date(date.getTime());
+                        }
+                        // has to do here
+                        updateInterest();
+                    }
+                },
+                mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH));
+        picker.show();
+    }
+
+    /**
+     * Update interest based on current values
+     */
+    private void updateInterest() {
+
+        String amountStr = mEditAmount.getText().toString();
+        String yieldStr = mEditYield.getText().toString();
+
+        if (mStartDate != null && mEndDate != null
+                && !Utils.isNullOrEmpty(amountStr)
+                && !Utils.isNullOrEmpty(yieldStr)) {
+            // update the interest value in UI
+            double days = Utils.getDiffDays(mStartDate, mEndDate);
+            double amount = Double.valueOf(amountStr);
+            double yield = Double.valueOf(yieldStr);
+            double interest = amount * (yield / 100) * (days / Constants.DAYS_OF_ONE_YEAR);
+            mEditInterest.setText(String.format(Locale.US, "%.2f", interest));
+            Log.d(Constants.LOG_TAG, "start = " + mStartDate.toString() + "\nend = " + mEndDate.toString()
+                    + "\ndays = " + days + "\namount = " + amount + "\nyield = " + yield + "\ninterest = " + interest);
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/eric/savingsmanager/utils/Utils.java
@@ -0,0 +1,59 @@
+package com.eric.savingsmanager.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by hshao on 10/04/2017.
+ */
+
+public class Utils {
+
+    /**
+     * Format a date to string with format
+     *
+     * @param date   date to format
+     * @param format format to follow
+     * @return formatted date string
+     */
+    public static String formatDate(Date date, String format) {
+        SimpleDateFormat formatter = new SimpleDateFormat(format, Locale.US);
+        return formatter.format(date);
+    }
+
+    /**
+     * Get the diff days between two dates
+     * @param start start date
+     * @param end end date
+     * @return days
+     */
+    public static int getDiffDays(Date start, Date end) {
+        Calendar fromCalendar = Calendar.getInstance();
+        fromCalendar.setTime(start);
+        fromCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        fromCalendar.set(Calendar.MINUTE, 0);
+        fromCalendar.set(Calendar.SECOND, 0);
+        fromCalendar.set(Calendar.MILLISECOND, 0);
+
+        Calendar toCalendar = Calendar.getInstance();
+        toCalendar.setTime(end);
+        toCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        toCalendar.set(Calendar.MINUTE, 0);
+        toCalendar.set(Calendar.SECOND, 0);
+        toCalendar.set(Calendar.MILLISECOND, 0);
+
+        return (int) ((toCalendar.getTime().getTime() - fromCalendar.getTime().getTime()) / (1000 * 60 * 60 * 24));
+    }
+
+    /**
+     * Whether string is empty or null
+     * @param str value
+     * @return emptyOrNull or not
+     */
+    public static boolean isNullOrEmpty(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+
+}
